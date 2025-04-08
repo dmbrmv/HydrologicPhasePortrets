a2ffece3305390c4412fc8f6e373850df8bb0e55
Dmitrii Abramov <dmbrmv96@gmail.com>
2025-04-09 00:15:58 +0300

    Initial commit with brief description of paper and some basic code structure

---
+++ notebooks/test.py
@@ -0,0 +1,14 @@
+# %% [markdown]
+# This is a markdown cell
+
+# %%
+def f(x):
+    return 3 * x + 1
+
+# %% [markdown]
+# This is another markdown cell
+
+# %%
+def g(x):
+    return 2 * x + 1
+# %%
---
+++ src/DataProcessing/some_file.py
@@ -0,0 +1,12 @@
+def test_function():
+    """A test function in the data processing module.
+    
+    This function prints a test message indicating that it is part of the data processing module.
+    """
+    # Print a test message
+    print("This is a test function in the data processing module.")
+    
+
+if __name__ == "__main__":
+    print("This is a placeholder for the data processing module.")
+
